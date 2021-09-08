5bd1c72f344be55cfee747591f5b150f05367138
Jonathan <vanillajonathan@users.noreply.github.com>
2021-09-08 20:41:18 +0200

    Create gruvbox.php
    Color constants for PHP.

---
+++ C/gruvbox.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Gruvbox colors.
+ */
+class Gruvbox
+{
+    const DARK0_HARD = "#1D2021";
+    const DARK0 = "#282828";
+    const DARK0_SOFT = "#32302F";
+    const DARK1 = "#3C3836";
+    const DARK2 = "#504945";
+    const DARK3 = "#665C54";
+    const DARK4 = "#7C6F64";
+
+    const GRAY_245 = "#928374";
+    const GRAY_244 = "#928374";
+
+    const LIGHT0_HARD = "#F9F5D7";
+    const LIGHT0 = "#FBF1C7";
+    const LIGHT0_SOFT = "#F2E5BC";
+    const LIGHT1 = "#EBDBB2";
+    const LIGHT2 = "#D5C4A1";
+    const LIGHT3 = "#BDAE93";
+    const LIGHT4 = "#A89984";
+
+    const BRIGHT_RED = "#FB4934";
+    const BRIGHT_GREEN = "#B8BB26";
+    const BRIGHT_YELLOW = "#FABD2F";
+    const BRIGHT_BLUE = "#83A598";
+    const BRIGHT_PURPLE = "#D3869B";
+    const BRIGHT_AQUA = "#8EC07C";
+    const BRIGHT_ORANGE = "#FE8019";
+
+    const NEUTRAL_RED = "#CC241D";
+    const NEUTRAL_GREEN = "#98971A";
+    const NEUTRAL_YELLOW = "#D79921";
+    const NEUTRAL_BLUE = "#458588";
+    const NEUTRAL_PURPLE = "#B16286";
+    const NEUTRAL_AQUA = "#689D6A";
+    const NEUTRAL_ORANGE = "#D65D0E";
+
+    const FADED_RED = "#9D0006";
+    const FADED_GREEN = "#79740E";
+    const FADED_YELLOW = "#B57614";
+    const FADED_BLUE = "#076678";
+    const FADED_PURPLE = "#8F3F71";
+    const FADED_AQUA = "#427B58";
+    const FADED_ORANGE = "#AF3A03";
+}
