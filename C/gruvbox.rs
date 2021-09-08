8a0960b83545ef9152550305a916f7ec75fe944f
Jonathan <vanillajonathan@users.noreply.github.com>
2021-09-08 20:34:15 +0200

    Create gruvbox.rs
    Color constants for Rust.

---
+++ C/gruvbox.rs
@@ -0,0 +1,44 @@
+//! Gruvbox colors.
+
+static DARK0_HARD: &str = "#1D2021";
+static DARK0: &str = "#282828";
+static DARK0_SOFT: &str = "#32302F";
+static DARK1: &str = "#3C3836";
+static DARK2: &str = "#504945";
+static DARK3: &str = "#665C54";
+static DARK4: &str = "#7C6F64";
+
+static GRAY_245: &str = "#928374";
+static GRAY_244: &str = "#928374";
+
+static LIGHT0_HARD: &str = "#F9F5D7";
+static LIGHT0: &str = "#FBF1C7";
+static LIGHT0_SOFT: &str = "#F2E5BC";
+static LIGHT1: &str = "#EBDBB2";
+static LIGHT2: &str = "#D5C4A1";
+static LIGHT3: &str = "#BDAE93";
+static LIGHT4: &str = "#A89984";
+
+static BRIGHT_RED: &str = "#FB4934";
+static BRIGHT_GREEN: &str = "#B8BB26";
+static BRIGHT_YELLOW: &str = "#FABD2F";
+static BRIGHT_BLUE: &str = "#83A598";
+static BRIGHT_PURPLE: &str = "#D3869B";
+static BRIGHT_AQUA: &str = "#8EC07C";
+static BRIGHT_ORANGE: &str = "#FE8019";
+
+static NEUTRAL_RED: &str = "#CC241D";
+static NEUTRAL_GREEN: &str = "#98971A";
+static NEUTRAL_YELLOW: &str = "#D79921";
+static NEUTRAL_BLUE: &str = "#458588";
+static NEUTRAL_PURPLE: &str = "#B16286";
+static NEUTRAL_AQUA: &str = "#689D6A";
+static NEUTRAL_ORANGE: &str = "#D65D0E";
+
+static FADED_RED: &str = "#9D0006";
+static FADED_GREEN: &str = "#79740E";
+static FADED_YELLOW: &str = "#B57614";
+static FADED_BLUE: &str = "#076678";
+static FADED_PURPLE: &str = "#8F3F71";
+static FADED_AQUA: &str = "#427B58";
+static FADED_ORANGE: &str = "#AF3A03";
